a21deecc40e58f2019ae3fcc9e3f2dbedc30522f
bscheibel <42147158+bscheibel@users.noreply.github.com>
2022-04-02 15:46:36 +0200

    Update tree_code.py

--- tree_code.py
+++ tree_code.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2021  Beate Scheibel
+# This file is part of edt.
+#
+# edt is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# edt is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# edt.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from sklearn.tree import _tree
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier as tree
